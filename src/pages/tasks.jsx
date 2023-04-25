25b1436e88619ee52e751717af58a575731a8bd6
Yosepha <47732841+Yosepha@users.noreply.github.com>
2023-04-24 21:43:28 -0500

    Add files via upload

---
+++ src/pages/tasks.jsx
@@ -0,0 +1,147 @@
+import { useRouter } from "next/navigation";
+import { Box, Typography } from "@mui/material";
+import React, {useState} from "react";
+import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+export default function Task() {
+  
+
+    const router = useRouter();
+    const redirectHome = () => {
+      router.push("home");
+    };
+    const signoutUser = () => {
+      router.push("login");
+    };
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
+  
+    const [showEdit, setShowEdit] = useState(-1);
+    const [updatedText, setUpdatedText] = useState("");
+  
+    // Helper Functions
+  
+    /* Adds a new item to the list array*/
+    function addItem() {
+      // ! Check for empty item
+      if (!newItem) {
+        alert("Press enter an item.");
+        return;
+      }
+  
+      const item = {
+        id: Math.floor(Math.random() * 1000),
+        value: newItem,
+      };
+  
+      // Add new item to items array
+      setItems((oldList) => [...oldList, item]);
+  
+      // Reset newItem back to original state
+      setNewItem("");
+    }
+  
+    /* Deletes an item based on the `item.id` key */
+    function deleteItem(id) {
+      const newArray = items.filter((item) => item.id !== id);
+      setItems(newArray);
+    }
+  
+    /* Edit an item text after creating it. */
+    function editItem(id, newText) {
+      // Get the current item
+      const currentItem = items.filter((item) => item.id === id);
+  
+      // Create a new item with same id
+      const newItem = {
+        id: currentItem.id,
+        value: newText,
+      };
+  
+      deleteItem(id);
+  
+      // Replace item in the item list
+      setItems((oldList) => [...oldList, newItem]);
+      setUpdatedText("");
+      setShowEdit(-1);
+    }
+  
+    // Main part of app
+    return (
+      
+      <div className="app">
+      {/* 1. Header  */}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" sx={{ flexGrow: 1 }}>
+              Collab
+            </Typography>
+            <Button color="inherit" onClick={signoutUser}>
+              Log Out
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <h1>Task List</h1>
+      
+      {/* 2. Add new item (input) */}
+        <input
+          type="text"
+          placeholder="Add an item..."
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+  
+        {/* Add (button) */}
+        <button onClick={() => addItem()}>Add</button>
+  
+        {/* 3. List of todos (unordered list) */}
+        <ul>
+          {items.map((item) => {
+            return (
+              <div>
+                <li key={item.id} onClick={() => setShowEdit(item.id)}>
+                  {item.value}
+                  <button
+                    className="delete-button"
+                    onClick={() => deleteItem(item.id)}
+                  >
+                    ❌
+                  </button>
+                </li>
+  
+                {showEdit == item.id ? (
+                  <div>
+                    <input
+                      type="text"
+                      value={updatedText}
+                      onChange={(e) => setUpdatedText(e.target.value)}
+                    />
+                    <button onClick={() => editItem(item.id, updatedText)}>
+                      Update
+                    </button>
+                  </div>
+                ) : null}
+              </div>
+            );
+          })}
+        </ul>
+  
+  
+  <Typography align="center" variant="h2" margin={"px10"} paddingTop={50}>
+    <Button
+      style={{ margin: "10px" }}
+      onClick={redirectHome}
+      variant="contained"
+      color="primary"
+    >
+      Home
+    </Button>
+  </Typography>
+  </div>
+  )
+}
