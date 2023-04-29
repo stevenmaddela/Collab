5d63efad4b28f4ebfffe4fb1d7ac69b1a3150388
trungpham1210 <77516675+trungpham1210@users.noreply.github.com>
2023-04-29 15:41:10 -0500

    Update UpdateProgress.js

--- src/UpdateProgress.js
+++ src/UpdateProgress.js
@@ -2,8 +2,8 @@ import { getDatabase, ref, update } from "firebase/database";
 import { db, auth } from "./firebaseConfig";
 
 // Update the progress of the given task to the new value.
-export function updateTaskProgress(userId, projectTitle, taskId, newProgress) {
-  update(ref(db, 'users/' + userId + '/projects/' + projectTitle+ '/tasks/' + taskId), {
+export function updateTaskProgress(userId, projectTitle, taskRef, newProgress) {
+  update(ref(db, 'users/' + userId + '/projects/' + projectTitle+ '/tasks/' + taskRef), {
     progress: newProgress,
   });
 }
