8b8d2f2adf573b1bc95dbca0ab41fdd01ed3dc3a
trungpham1210 <77516675+trungpham1210@users.noreply.github.com>
2023-04-29 15:16:17 -0500

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -2,7 +2,7 @@ import { getDatabase, ref, push } from "firebase/database";
 import { db, auth } from "./firebaseConfig";
 
 export function createTask(userId, projectTitle, task) {
-  const taskRef = push(ref(db, `users/'+ userId + '/projects/' + projectTitle + '/tasks`));
+  const taskRef = push(ref(db, 'users/'+ userId + '/projects/' + projectTitle + '/tasks'));
   taskRef.set({
     ...task,
     createdAt: new Date().getTime(),
