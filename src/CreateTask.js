0a7dafbc14760cc45148bcfe6468957f20355ebe
trungpham1210 <77516675+trungpham1210@users.noreply.github.com>
2023-04-30 16:04:46 -0500

    Create CreateTask.js

---
+++ src/CreateTask.js
@@ -0,0 +1,10 @@
+import { getDatabase, ref, set } from "firebase/database";
+import { db, auth } from "./firebaseConfig";
+
+export function createTaskWithTitle(userId, taskTitle) {
+  set(ref(db, 'users/'+ userId+ '/tasks' + taskTitle), {
+    title: taskTitle,
+    members: [userId]
+  });
+}
+
