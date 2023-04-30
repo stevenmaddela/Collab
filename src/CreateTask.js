1486479c3ba69a580b3b1160b0a3730d586a4e1b
trungpham1210 <77516675+trungpham1210@users.noreply.github.com>
2023-04-29 22:27:24 -0500

    Update CreateTask.js

--- src/CreateTask.js
+++ src/CreateTask.js
@@ -1,15 +1,13 @@
 import { getDatabase, ref, push } from "firebase/database";
 import { db, auth } from "./firebaseConfig";
 
-export function createTaskWithTitle(userId, projectTitle, title, dueDate, assignedTo) {
-  const taskRef = push(ref(db, 'users/' + userId '/projects/' +  projectTitle '/tasks'));
+export function createTaskWithTitle(taskTitle) {
+  const userId = auth.currentUser.uid;
+  const taskRef = push(ref(db, 'users/'+ userId+ '/tasks'));
   taskRef.set({
-    title,
-    dueDate,
-    assignedTo,
-    progress: 'Not started',
-    createdAt: new Date().getTime(),
-    createdBy: auth.currentUser.uid,
+    title: taskTitle,
+    //createdAt: new Date().getTime(),
+    createdBy: userId,
   });
   return taskRef.key;
 }
